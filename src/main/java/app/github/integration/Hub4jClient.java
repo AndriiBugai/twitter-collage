6acc54681cac8e523ed7be6af5af6a540ce5ee1c
Andrii Bugai <andriibugai@yahoo.com>
2026-02-12 15:18:22 +0100

    load images in parallel

--- src/main/java/app/github/integration/Hub4jClient.java
+++ src/main/java/app/github/integration/Hub4jClient.java
@@ -5,7 +5,6 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,18 +21,19 @@ public class Hub4jClient {
     }
 
     public List<BufferedImage> getAvatars(int collageSize,
-                                          String gitHubName) throws MalformedURLException, IOException {
+                                          String username) throws IOException {
+        final String githubToken = System.getenv("GITHUB_TOKEN");
+        final GitHub github = new GitHubBuilder().withOAuthToken(githubToken).build();
+        final GHUser githubUser = github.getUser(username);
 
-        final String token = System.getenv("GITHUB_TOKEN");
-
-        final GitHub github = new GitHubBuilder().withOAuthToken(token).build();
-
-        final GHUser user = github.getUser(gitHubName);
-
-        return StreamSupport
-                .stream(user.listFollowers().spliterator(), false)
+        List<String> avatarsUrl = StreamSupport
+                .stream(githubUser.listFollowers().spliterator(), false)
                 .limit(collageSize)
                 .map(GHPerson::getAvatarUrl)
+                .toList();
+
+        // load images in parallel
+        return avatarsUrl.parallelStream()
                 .map(this::loadImage)
                 .collect(Collectors.toList());
     }
